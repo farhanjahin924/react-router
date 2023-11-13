3835313575275a6704058f8d092f4707ae84a15f
Farhan Jahin <farhan15-13948@diu.edu.bd>
2023-11-14 00:15:53 +0600

    nevigator

--- src/Components/ShowDetails/ShowDetails.jsx
+++ src/Components/ShowDetails/ShowDetails.jsx
@@ -1,8 +1,10 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 
 const ShowDetails = () => {
     const details =useLoaderData();
+    const nevigate = useNavigate();
+
     const {id, name, email ,username,address} = details;
     //here address is a object! how to fix this ?
     const { street, city, suite , zipcode } = address || {};
@@ -10,7 +12,12 @@ const ShowDetails = () => {
         border : '2px solid black',
         margin : "5px"
     }
-
+    const btnhandle = () =>{
+        nevigate(`/users`) 
+    }
+    const testMinusOne = () =>{
+        nevigate(-1)
+    }
     return (
         <div style={userStyle}>
             <h1>User Details : </h1>
@@ -23,6 +30,8 @@ const ShowDetails = () => {
           <p>City: {city}</p>
           <p> {suite}</p>
           <p>Zip: {zipcode}</p>
+          <button  onClick={btnhandle}>Go back</button>
+          <button onClick={testMinusOne}>-1</button>
         </div>
     );
 };
